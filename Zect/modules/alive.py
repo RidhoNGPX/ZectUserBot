de0bb72f5ede5048a4d285e3861673711e0ad4a9
Ridho <89362181+RidhoNGPX@users.noreply.github.com>
2022-01-03 23:18:45 +0700

    Update alive.py

--- Zect/modules/alive.py
+++ Zect/modules/alive.py
@@ -63,12 +63,12 @@ def get_readable_time(seconds: int) -> str:
 async def alive(_, m):
     start_time = time.time()
     uptime = get_readable_time((time.time() - StartTime))
-    reply_msg = f"**[Zect](https://github.com/okay-retard/ZectUserBot)**\n"
+    reply_msg = f"**[Source](https://github.com/RidhoNGPX/ZectUserBot)**\n"
     reply_msg += f"__Python__: `{__python_version__}`\n"
     reply_msg += f"__@Pyrogram version__: `{__pyro_version__}`\n"
     end_time = time.time()
-    reply_msg += f"__Zect uptime__: {uptime}"
-    photo = "https://telegra.ph//file/dcd069c3eb26c9fa3011d.jpg"
+    reply_msg += f"__uptime__: {uptime}"
+    photo = "https://telegra.ph/file/191a64df5fd14d60b383b.jpg"
     await m.delete()
     if m.reply_to_message:
         await app.send_photo(
