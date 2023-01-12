10e9d4c6253ea864b30077cf7e7dcf05712c26cc
Ridho <89362181+RidhoNGPX@users.noreply.github.com>
2023-01-12 11:23:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 API_HASH = os.getenv("API_HASH")
 API_ID = int(os.getenv("API_ID"))
 MONGO_URI = os.getenv("MONGO_URI")
 SESSION = os.getenv("SESSION")
 PREFIX = os.getenv("PREFIX")
 LOG_CHAT = int(os.getenv("LOG_CHAT"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
