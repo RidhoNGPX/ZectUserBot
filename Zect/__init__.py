fcf03544db796a2c625b6eb056303f6dbbd2cf5d
Ridho <89362181+RidhoNGPX@users.noreply.github.com>
2023-01-12 11:29:09 +0700

    Update __init__.py

--- Zect/__init__.py
+++ Zect/__init__.py
@@ -12,9 +12,7 @@ import time
 from pyrogram import Client, errors
 from config import API_HASH, API_ID, SESSION, BOT_TOKEN
 import logging
-from sqlalchemy import create_engine, exc
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
+
 
 logging.basicConfig(
     filename="app.txt",
@@ -35,20 +33,4 @@ SESSION = SESSION
 app = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
 bot = Client("Bot", bot_token=BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH)
 
-DB_AVAILABLE = False
- 
-# Postgresql
-def mulaisql() -> scoped_session:
-    global DB_AVAILABLE
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
-    BASE.metadata.bind = engine
-    try:
-        BASE.metadata.create_all(engine)
-    except exc.OperationalError:
-        DB_AVAILABLE = False
-        return False
-    DB_AVAILABLE = True
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-BASE = declarative_base()
-SESSION = mulaisql()
